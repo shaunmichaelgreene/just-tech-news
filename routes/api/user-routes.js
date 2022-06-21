c484c29d673195f60d7a4ee3aa55afacc9f977ee
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-21 18:30:25 -0400

    mid 13.1.6 get and post actions added for user model

---
+++ routes/api/user-routes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//GET /api/users
+router.get('/', (req, res) => {
+    //access our User model and run .findAll() method
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//GET /api/users/1
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+        
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//POST /api/users
+router.post('/', (req, res) => {
+    //expects {username: 'shaunmichaelgreene', email: 'shaunmichaelgreene@gmail.com', password: 'shauncodes1986'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//PUT /api/users/1
+router.put('/:id', (req, res) => {});
+
+//DELETE /api/users/1
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
