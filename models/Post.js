e41483c0e8b2045d3411daffc6139412ee25440f
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-26 23:22:18 -0400

    end 13.4.3 vote model added

--- models/Post.js
+++ models/Post.js
@@ -1,42 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-// const User = require('./User');
-
-//create Post model
+// create our Post model
 class Post extends Model {}
 
-//create fields/columns for Post model
-Post.init({
+// create fields/columns for Post model
+Post.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     title: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     post_url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            isURL: true
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
+      }
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
     }
-},
-{
+  },
+  {
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: 'post'
-});
+  }
+);
 
 module.exports = Post;
